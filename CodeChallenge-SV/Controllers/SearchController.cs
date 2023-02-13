ccba8e7ad8db83c59d72c4943d2719b8eab7fda6
MethMachine <juliangrosjean@aol.com>
2023-02-13 16:18:55 +0100

    Add project files.

---
+++ CodeChallenge-SV/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge_SV.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly ILogger<SearchController> _logger;
+
+        public SearchController(ILogger<SearchController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
