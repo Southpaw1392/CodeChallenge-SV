46b49a3c122f7ef727f7d81d986e38442ab0420a
MethMachine <juliangrosjean@aol.com>
2023-02-13 18:12:16 +0100

    added first try to calculate weight for buildings

--- CodeChallenge-SV/BusinessLogicLayer/SearchBll.cs
+++ CodeChallenge-SV/BusinessLogicLayer/SearchBll.cs
@@ -24,11 +24,49 @@ namespace CodeChallenge_SV.BusinessLogicLayer
             return await FilterAndSortData(searchInput, buildings, groups, locks, media);
         }
 
-        private async Task<List<SearchResult>> FilterAndSortData(string searchInput, IEnumerable<Building> buildings, IEnumerable<Group> groups, IEnumerable<Lock> locks, IEnumerable<Medium> media)
+        private async Task<List<SearchResult>> FilterAndSortData(string searchInput, List<Building> buildings, List<Group> groups, List<Lock> locks, List<Medium> media)
         {
             List<SearchResult> results = new List<SearchResult>();
-            //TODO: Implement algorithm to calculate weight
-            return results;
+            ProcessBuildings(searchInput, buildings, results);
+
+            return results.OrderByDescending(x => x.Weight).ThenBy(n => n.Name).ToList();
+        }
+
+        private static void ProcessBuildings(string searchInput, List<Building> buildings, List<SearchResult> results)
+        {
+            if (buildings != null && buildings.Any())
+            {
+                foreach (var building in buildings)
+                {
+                    int weight = 0;
+                    if (building.Name.Contains(searchInput))
+                    {
+                        weight += (building.Name == searchInput) ? 9 * 10 : 9;
+                    }
+
+                    if (building.ShortCut.Contains(searchInput))
+                    {
+                        weight += (building.ShortCut == searchInput) ? 7 * 10 : 7;
+                    }
+
+                    if (building.Description != null && building.Description.Contains(searchInput))
+                    {
+                        weight += (building.Description == searchInput) ? 5 * 10 : 5;
+                    }
+
+                    if (weight > 0)
+                    {
+                        results.Add(new SearchResult
+                        {
+                            Weight = weight,
+                            EntityType = "Building",
+                            Name = building.Name,
+                            ShortCut = building.ShortCut,
+                            Description = building.Description
+                        });
+                    }
+                }
+            }
         }
     }
 }
--- CodeChallenge-SV/Dtos/SearchResult.cs
+++ CodeChallenge-SV/Dtos/SearchResult.cs
@@ -5,11 +5,11 @@
         public int Weight { get; set; }
         public string EntityType { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string BuildingShortCut { get; set; }
+        public string? Description { get; set; }
+        public string ShortCut { get; set; }
         public string Type { get; set; }
         public string SerialNumber { get; set; }
-        public string Floor { get; set; }
+        public string? Floor { get; set; }
         public string RoomNumber { get; set; }
         public string Owner { get; set; }
     }
