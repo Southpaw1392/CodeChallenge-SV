9306cc68516adafa30620c71d33ebfe010353ebb
MethMachine <juliangrosjean@aol.com>
2023-02-19 12:58:27 +0100

    added EntityFrameworkCore.InMemory and SearchContext

---
+++ CodeChallenge-SV/Data/SearchContext.cs
@@ -0,0 +1,23 @@
+﻿using CodeChallenge_SV.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge_SV.Data
+{
+    public class SearchContext : DbContext
+    {
+        public SearchContext(DbContextOptions<SearchContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Building> Buildings { get; set; } = null!;
+        public DbSet<Group> Groups { get; set; } = null!;
+        public DbSet<Lock> Locks { get; set; } = null!;
+        public DbSet<Medium> Media { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Search");
+        }
+    }
+}
--- CodeChallenge-SV/Program.cs
+++ CodeChallenge-SV/Program.cs
@@ -1,13 +1,17 @@
 using CodeChallenge_SV.BusinessLogicLayer;
+using CodeChallenge_SV.Data;
 using CodeChallenge_SV.DataAccessLayer;
 using CodeChallenge_SV.DataAccessLayerInteraces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 const string allowedOriginSetting = "AllowedOrigin";
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SearchContext>(opt =>
+    opt.UseInMemoryDatabase("Search"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
