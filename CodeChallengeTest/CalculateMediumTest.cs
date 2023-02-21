37a8e3a3a7ee5dde869f754e8872d1ab5ce00835
MethMachine <juliangrosjean@aol.com>
2023-02-21 18:02:06 +0100

    unit test for medium weight calculation

---
+++ CodeChallengeTest/CalculateMediumTest.cs
@@ -0,0 +1,83 @@
+﻿using CodeChallenge_SV.BusinessLogicLayer;
+using CodeChallenge_SV.Dtos;
+using CodeChallenge_SV.Models;
+
+namespace CodeChallengeTest
+{
+    public class CalculateMediumTest
+    {
+        [Fact]
+        public void CalculateMediumWeight()
+        {
+            // Arrange
+            string searchInput = "Max Mustermann";
+            string[] searchWords = { "Max Mustermann" };
+            var results = new List<SearchResult>();
+            var medium = new Medium
+            {
+                Owner = "Max Mustermann",
+                SerialNumber = "123456",
+                Description = "Max Mustermanns Transponder",
+                Type = "Transponder"
+            };
+
+            // Act
+            SearchBll.CalculateMediumWeight(searchInput, searchWords, medium, ref results);
+
+            // Assert
+            Assert.Equal(106, results[0].Weight);
+        }
+
+        [Fact]
+        public void CalculateTransitiveMediumWeight()
+        {
+            // Arrange
+            string searchInput = "Max";
+            string[] searchWords = { "Max" };
+            var results = new List<SearchResult>();
+            var medium = new Medium
+            {
+                Owner = "Max Mustermann",
+                SerialNumber = "123456",
+                Description = "Max Mustermanns Transponder",
+                Type = "Transponder",
+                Group = new Group
+                {
+                    Name = "Max Mustermanns Gruppe"
+                }
+            };
+
+            // Act
+            SearchBll.CalculateMediumWeight(searchInput, searchWords, medium, ref results);
+
+            // Assert
+            Assert.Equal(24, results[0].Weight);
+        }
+
+        [Fact]
+        public void SetEntityTypeToMedium()
+        {
+            // Arrange
+            string searchInput = "Max";
+            string[] searchWords = { "Max" };
+            var results = new List<SearchResult>();
+            var medium = new Medium
+            {
+                Owner = "Max Mustermann",
+                SerialNumber = "123456",
+                Description = "Max Mustermanns Transponder",
+                Type = "Transponder",
+                Group = new Group
+                {
+                    Name = "Max Mustermanns Gruppe"
+                }
+            };
+
+            // Act
+            SearchBll.CalculateMediumWeight(searchInput, searchWords, medium, ref results);
+
+            // Assert
+            Assert.Equal("Medium", results[0].EntityType);
+        }
+    }
+}
