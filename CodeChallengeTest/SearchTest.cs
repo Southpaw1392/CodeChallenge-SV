8c409f57466d2fbaab42de9eaa00391cef289428
MethMachine <juliangrosjean@aol.com>
2023-02-22 18:02:51 +0100

    added a test for FilterAndSortData, added moq and fluent assertions

---
+++ CodeChallengeTest/SearchTest.cs
@@ -0,0 +1,158 @@
+﻿using CodeChallenge_SV.BusinessLogicLayer;
+using CodeChallenge_SV.Controllers;
+using CodeChallenge_SV.DataAccessLayer;
+using CodeChallenge_SV.DataAccessLayerInteraces;
+using CodeChallenge_SV.Dtos;
+using CodeChallenge_SV.Models;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeChallengeTest
+{
+    public class SearchTest /*: IClassFixture<ISearchDal>*/
+    {
+        //private readonly SearchBll _searchBll;
+        //public SearchTest(ISearchDal searchDal)
+        //{
+            
+        //    _searchBll = new SearchBll(searchDal);
+        //}
+
+        private ISearchDal getMockSearchDAL() 
+        {
+            Mock<ISearchDal> mockObject = new Mock<ISearchDal>();
+            mockObject.Setup(m => m.GetBuildingsBySearchInput(new string[] { "Test" })).Returns(Task.FromResult(new List<Building>()));
+            mockObject.Setup(m => m.GetGroupsBySearchInput(new string[] { "Test" })).Returns(Task.FromResult(new List<Group>()));
+            mockObject.Setup(m => m.GetLocksBySearchInput(new string[] { "Test" })).Returns(Task.FromResult(new List<Lock>()));
+            mockObject.Setup(m => m.GetMediaBySearchInput(new string[] { "Test" })).Returns(Task.FromResult(new List<Medium>()));
+            return mockObject.Object;
+        }
+
+        [Fact]
+        public async Task FilterAndSortData()
+        {
+            ISearchDal searchDal = getMockSearchDAL();
+            SearchBll searchBll = new SearchBll(searchDal);
+
+            var searchInput = "Test";
+            var searchWords = new string[] { "Test" };
+            var buildings = new List<Building>()
+            {
+                new Building {
+                    ShortCut = "Test",
+                    Name = "Test Office",
+                    Description = "Test Office, Test Street 1, 12345 Testtown"
+                },
+                new Building {
+                    ShortCut = "HQ",
+                    Name = "Head Quarters",
+                    Description = "Head Quarters, HQ Street 7, 12345 Testtown"
+                },
+            };
+
+            var groups = new List<Group>()
+            {
+                new Group {
+                    Name = "Blue",
+                    Description = "Members are required to wear a blue shirt"
+                },
+                new Group {
+                    Name = "Red",
+                    Description = "Members are required to wear a red shirt"
+                },
+            };
+
+            var locks = new List<Lock>()
+            {
+                new Lock
+                {
+                    Name = "Testroom",
+                    Type = "Cylinder",
+                    SerialNumber = "123456",
+                    Floor = "1.OG",
+                    RoomNumber = "404",
+                    Description = "This is the lock for room 404",
+                    Building = new Building
+                    {
+                        ShortCut = "HQ",
+                        Name = "Head Quarters",
+                    }
+                },
+                new Lock
+                {
+                    Name = "Toilets",
+                    Type = "Cylinder",
+                    SerialNumber = "654321",
+                    Floor = "2.OG",
+                    RoomNumber = "42",
+                    Description = null
+                }
+            };
+
+            var media = new List<Medium>()
+            {
+                new Medium {
+                    Owner = "Max Mustermann",
+                    SerialNumber = "123456",
+                    Description = "Max Mustermanns Transponder",
+                    Type = "Transponder",
+                    Group = new Group
+                    {
+                        Name = "Max Mustermanns Gruppe"
+                    }
+                },
+                new Medium 
+                {
+                    Owner = "Olaf Olafson",
+                    SerialNumber = "654321",
+                    Description = null,
+                    Type = "Transponder"
+                },
+            };
+
+            var expected = new List<SearchResult>()
+            {
+                new SearchResult
+                {
+                    Weight = 84,
+                    EntityType = "Building",
+                    Name = "Test Office",
+                    Description = "Test Office, Test Street 1, 12345 Testtown",
+                    ShortCut = "Test"
+                },
+                new SearchResult
+                {
+                    Weight = 10,
+                    Name = "Testroom",
+                    EntityType = "Lock",
+                    Type = "Cylinder",
+                    SerialNumber = "123456",
+                    Floor = "1.OG",
+                    RoomNumber = "404",
+                    Description = "This is the lock for room 404"
+                },
+                new SearchResult
+                {
+                    Weight = 5,
+                    EntityType = "Building",
+                    Name = "Head Quarters",
+                    ShortCut = "HQ",
+                    Description = "Head Quarters, HQ Street 7, 12345 Testtown"
+                },
+            };
+
+            // Act
+            List<SearchResult> result = await searchBll.FilterAndSortData(searchInput, searchWords, buildings, groups, locks, media);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
